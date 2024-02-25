67f9e39da1cc171adefc384c487e529e4d8e4dc7
Nehavishwakarma1 <159677296+Nehavishwakarma1@users.noreply.github.com>
2024-02-25 09:58:16 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+   
+    var helloSpeaker = {};
+    helloSpeaker.speak = function(name) {
+      console.log("Hello " + name);
+    };
+  
+    
+    var byeSpeaker = {};
+    byeSpeaker.speak = function(name) {
+      console.log("Good Bye " + name);
+    };
+  
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  
+  })();
